c0939ce5155bcfd4a256ff1a4c724a47b007b54d
tahamhamedi693-ui <tahamhamedi693@gmail.com>
2025-09-13 22:23:00 +0100

    Fix Stripe web bundling error by renaming native file

--- components/StripeProviderWrapper.tsx
+++ components/StripeProviderWrapper.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import { Platform } from 'react-native';
-
-// Only import Stripe on native platforms
-let StripeProvider: any = null;
-if (Platform.OS !== 'web') {
-  StripeProvider = require('@stripe/stripe-react-native').StripeProvider;
-}
+import { StripeProvider } from '@stripe/stripe-react-native';
 
 interface StripeProviderWrapperProps {
   publishableKey: string;
@@ -13,11 +7,6 @@ interface StripeProviderWrapperProps {
 }
 
 export default function StripeProviderWrapper({ publishableKey, children }: StripeProviderWrapperProps) {
-  // Prevent importing Stripe on web platform
-  if (Platform.OS === 'web') {
-    return <>{children}</>;
-  }
-
   return (
     <StripeProvider publishableKey={publishableKey}>
       {children}
