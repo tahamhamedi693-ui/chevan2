523347d7b0a0ccd4ea203b59dc0dda963f115167
tahamhamedi693-ui <tahamhamedi693@gmail.com>
2025-09-13 22:29:46 +0100

    Fix Stripe web bundling error by removing problematic alias

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,9 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-// Exclude @stripe/stripe-react-native from web builds
 config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
-
-// Alias native modules to web-compatible wrappers
-config.resolver.extraNodeModules = {
-  '@stripe/stripe-react-native': path.resolve(__dirname, 'components/StripeProviderWrapper.web.tsx'),
-};
 
 // Platform-specific extensions priority
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'web.js', 'web.ts', 'web.tsx'];
